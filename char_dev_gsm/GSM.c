865d0e0400cd8cb0a613c5b57fb5829c9b82623d
guruprasad-92 <prusty.guruprasad92@gmail.com>
2019-10-21 20:39:58 +0530

    commit-2 (modular)

--- char_dev_gsm/GSM.c
+++ char_dev_gsm/GSM.c
@@ -5,13 +5,13 @@
 #include <linux/init.h> /* module_init(), module_exit() */
 #include <linux/module.h> /* version info, MODULE_LICENSE, MODULE_AUTHOR, printk() */
 #include <linux/kernel.h> // Contains types, macros, functions for the kernel
-#include "device.h"
+#include "src/device/device.h"
 
-MODULE_DESCRIPTION("GSM driver");
-MODULE_LICENSE("Dual BSD/GPL");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Guruprasad");
 MODULE_DESCRIPTION("A simple driver");
 MODULE_VERSION("0.1");
+
 
 static char *name = "GSM";        ///< An example LKM argument -- default value is "world"
 module_param(name, charp, S_IRUGO); ///< Param desc. charp = char ptr, S_IRUGO can be read/not changed
@@ -32,8 +32,8 @@ static void __exit gsm_exit (void)
 {
     printk(KERN_INFO "GSM: Removing the %s module.\n",name);
     unregister_dev();
+    
 }
-
 
 module_init(gsm_init);
 module_exit(gsm_exit);
